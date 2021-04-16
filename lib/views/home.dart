216b8428d249c73a193f7e153ac06e6944a5dcf1
paedayz <pae16phasit@gmail.com>
2021-04-16 18:13:10 +0700

    show search user function and list tile

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:messeger_clone/services/auth.dart';
+import 'package:messeger_clone/services/database.dart';
 import 'package:messeger_clone/views/signin.dart';
 
 class Home extends StatefulWidget {
@@ -10,8 +12,57 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   bool isSearching = false;
 
+  Stream userStream;
+
   TextEditingController searchUsernameEditingController =
       TextEditingController();
+
+  onSearchButtonClick() async {
+    setState(() {
+      isSearching = true;
+    });
+    userStream = await DatabaseMethods()
+        .getUserByUsername(searchUsernameEditingController.text);
+    setState(() {});
+  }
+
+  Widget searchUsersList() {
+    return StreamBuilder(
+      builder: (context, snapshot) {
+        return snapshot.hasData
+            ? ListView.builder(
+                shrinkWrap: true,
+                itemCount: snapshot.data.docs.length,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot ds = snapshot.data.docs[index];
+                  return searchListUserTile(
+                      ds['imgUrl'], ds['username'], ds['email']);
+                },
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              );
+      },
+      stream: userStream,
+    );
+  }
+
+  Widget chatRoomList() {
+    return Container();
+  }
+
+  Widget searchListUserTile(String profileUrl, name, email) {
+    return Row(
+      children: [
+        Image.network(
+          profileUrl,
+          height: 30,
+          width: 30,
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -77,9 +128,9 @@ class _HomeState extends State<Home> {
                           ),
                           GestureDetector(
                             onTap: () {
-                              setState(() {
-                                isSearching = true;
-                              });
+                              if (searchUsernameEditingController.text != '') {
+                                onSearchButtonClick();
+                              }
                             },
                             child: Icon(Icons.search),
                           )
@@ -88,7 +139,8 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                 ],
-              )
+              ),
+              isSearching ? searchUsersList() : chatRoomList()
             ],
           ),
         ),
