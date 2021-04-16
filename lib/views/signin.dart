52dfe2d474693db36bd1fc6e5250ceb7833d20d5
paedayz <pae16phasit@gmail.com>
2021-04-16 15:46:23 +0700

    fix multidex bug

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -10,6 +10,22 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Messenger Clone')),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(24),
+            color: Color(0xffDB4437),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Text(
+            "Sign In with Google",
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
